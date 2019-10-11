9088f922accb3e076cb85a6629e6bb36aceaef74
zy <dake0805@gmail.com>
2019-10-11 17:36:54 +0800

    finish ex3

---
+++ ex3/src/lesson/j2ee/ex3/ShowCartTag.java
@@ -0,0 +1,45 @@
+package lesson.j2ee.ex3;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+public class ShowCartTag extends SimpleTagSupport {
+    @Override
+    public void doTag() throws JspException, IOException {
+        super.doTag();
+        PageContext pageContext = (PageContext) getJspContext();
+        HttpSession httpSession = pageContext.getSession();
+        Integer itemCount = (Integer) httpSession.getAttribute("itemCount");
+        // 如果session是新的
+        if (itemCount == null)
+            itemCount = 0;
+
+
+        // 接收传来的参数
+        String[] itemsSelected;
+        String itemName;
+        itemsSelected = pageContext.getRequest().getParameterValues("item");
+
+        JspWriter jspWriter = getJspContext().getOut();
+        if (itemsSelected != null) {
+            for (String s : itemsSelected) {
+                itemName = s;
+                System.out.println(itemName);
+                itemCount = itemCount + 1;
+                // 购买的条目
+                httpSession.setAttribute("item" + itemCount, itemName);
+                // 总条目
+                httpSession.setAttribute("itemCount", itemCount);
+            }
+        }
+
+        for (int i = 1; i <= itemCount; i++) {
+            jspWriter.println((String) httpSession.getAttribute("item" + i) + "<hr>");
+        }
+        jspWriter.close();
+    }
+}
---
+++ ex3/web/ShowCart.jsp
@@ -0,0 +1,23 @@
+<%@ taglib prefix="mytag" uri="MyFirstTag" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: zy
+  Date: 10/11/2019
+  Time: 5:15 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<title>
+    item list
+</title>
+<body>
+<h4>
+    Session List:
+</h4>
+<hr>
+<br><br>
+<mytag:showCartTag/>
+</body>
+</html>
