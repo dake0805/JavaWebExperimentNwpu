b661b40bcdbe4621365ab4df001a086bf27126c8
zy <dake0805@gmail.com>
2019-10-27 12:05:14 +0800

    ex4.1

--- ex4/src/lesson/j2ee/ex4/MyRequestFilter.java
+++ ex4/src/lesson/j2ee/ex4/MyRequestFilter.java
@@ -2,19 +2,42 @@ package lesson.j2ee.ex4;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "MyRequestFilter")
 public class MyRequestFilter implements Filter {
+
+    private FilterConfig filterConfig = null;
+
     public void destroy() {
+        this.filterConfig = null;
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp);
+        HttpServletRequest httpServletRequest = (HttpServletRequest) req;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) resp;
+
+        String redirectPath = httpServletRequest.getContextPath() + filterConfig.getInitParameter("redirectPath");
+
+        String disableFilter = filterConfig.getInitParameter("disableFilter");
+
+        if (disableFilter.toUpperCase().equals("Y")) {
+            chain.doFilter(req, resp);
+        }
+        if (disableFilter.toUpperCase().equals("N")) {
+            if (httpServletRequest.getRequestURI().contains("/SelectBeer.jsp")) {
+                chain.doFilter(req, resp);
+            } else {
+                httpServletResponse.sendRedirect(redirectPath);
+            }
+        } else {
+            chain.doFilter(req, resp);
+        }
     }
 
     public void init(FilterConfig config) throws ServletException {
-
+        this.filterConfig = config;
     }
 
 }
